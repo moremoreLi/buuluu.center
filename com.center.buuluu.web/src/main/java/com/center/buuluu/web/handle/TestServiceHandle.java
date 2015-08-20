381c8a3d5b11a758e14fac67af568cd4d9a7efb2
more <more.Li@bluepay.asia>
2015-08-20 18:35:01 +0800

    testme

--- com.center.buuluu.web/src/main/java/com/center/buuluu/web/handle/TestServiceHandle.java
+++ com.center.buuluu.web/src/main/java/com/center/buuluu/web/handle/TestServiceHandle.java
@@ -1,5 +1,6 @@
 package com.center.buuluu.web.handle;
 
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
