199ea63858f418f35727c3ce16f72e09b8fc77cb
more <more.Li@bluepay.asia>
2015-08-25 12:27:03 +0800

    update

---
+++ com.user.buuluu.service/src/main/java/com/user/buuluu/common/exception/BuuluuAPIException.java
@@ -0,0 +1,39 @@
+package com.user.buuluu.common.exception;
+
+import com.user.buuluu.common.util.CodeStatus;
+
+/**
+ * 
+ * @author More
+ * 2015-7-16
+ */
+public  class BuuluuAPIException extends RuntimeException {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7065579710316348767L;
+
+	private int errorCode = CodeStatus.SYSTEM_ERROR;
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public BuuluuAPIException() {
+		super();
+	}
+
+	public BuuluuAPIException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BuuluuAPIException(String message) {
+		super(message);
+	}
+
+	public BuuluuAPIException(Throwable cause) {
+		super(cause);
+	}
+
+}
