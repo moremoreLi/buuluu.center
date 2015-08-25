a850af3a171fd9c5f062c1dc4895e62f7d992630
more <more.Li@bluepay.asia>
2015-08-25 13:39:50 +0800

    update

---
+++ com.user.buuluu.web/src/main/java/com/user/buuluu/web/controller/webservice/UserSeviceController.java
@@ -0,0 +1,20 @@
+package com.user.buuluu.web.controller.webservice;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+ * @author More
+ * 用户的相关接口
+ * 2015.8.25
+ *
+ */
+@Controller
+public class UserSeviceController {
+
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	
+}
