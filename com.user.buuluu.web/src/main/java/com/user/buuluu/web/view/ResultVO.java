5224c79326d7c7ca978e6c8e77b91edf9ebcafa9
more <more.Li@bluepay.asia>
2015-08-25 10:31:57 +0800

    update

---
+++ com.user.buuluu.web/src/main/java/com/user/buuluu/web/view/ResultVO.java
@@ -0,0 +1,37 @@
+package com.user.buuluu.web.view;
+
+import com.user.buuluu.common.util.CodeStatus;
+
+public class ResultVO<T> {
+
+	private String status = CodeStatus.SUCCESS;
+	private String errorMessage = "";
+	/*JSON 返回做了+ -分數 主鍵ID*/
+	private T result;
+
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	public T getResult() {
+		return result;
+	}
+
+	public void setResult(T result) {
+		this.result = result;
+	}
+
+}
